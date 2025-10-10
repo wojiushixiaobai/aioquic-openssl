107757823165a1a25511e84b44a15d659c271f4b
Jeremy Lainé <jeremy.laine@m4x.org>
2025-10-10 22:49:33 +0200

    Update OpenSSL to the latest LTS version (3.5.4)

--- scripts/build-openssl.py
+++ scripts/build-openssl.py
@@ -68,7 +68,7 @@ output_dir = os.path.abspath("output")
 if platform.system() == "Linux" and os.environ.get("CIBUILDWHEEL") == "1":
     output_dir = "/output"
     try:
-        run(["yum", "-y", "install", "perl-IPC-Cmd"])
+        run(["yum", "-y", "install", "perl-IPC-Cmd", "perl-Time-Piece"])
     except Exception:
         # Alpine Linux doesn't use yum.
         pass
@@ -81,7 +81,7 @@ if not os.path.exists(output_tarball):
     os.chdir(build_dir)
 
     # build openssl
-    extract("openssl", "https://www.openssl.org/source/openssl-3.4.1.tar.gz")
+    extract("openssl", "https://www.openssl.org/source/openssl-3.5.4.tar.gz")
     os.chdir("openssl")
     run(["./config"] + configure_args + ["no-apps", "no-comp", "no-dso", "no-shared", "no-tests"])
     run(["make"])
